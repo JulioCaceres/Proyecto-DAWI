9d4534309e3ead5ccac101c558bb1f789d0097e3
JulioCaceres <juliocarloscaceres1904@gmail.com>
2023-06-27 11:14:22 -0500

    Ultimate BD

--- BaseDeDatos/bd_sistemamatricula27062023.sql
+++ BaseDeDatos/bd_sistemamatricula27062023.sql
@@ -26,12 +26,12 @@ DROP TABLE IF EXISTS `tb_alumno`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_alumno` (
   `codigo_alum` varchar(5) NOT NULL,
-  `nombre_alum` varchar(255) DEFAULT NULL,
-  `apellido_alum` varchar(255) DEFAULT NULL,
-  `numerodocumento_alum` varchar(255) DEFAULT NULL,
-  `nacionalidad_alum` varchar(255) DEFAULT NULL,
-  `correo_alum` varchar(255) DEFAULT NULL,
-  `celular_alum` varchar(255) DEFAULT NULL,
+  `nombre_alum` varchar(45) NOT NULL,
+  `apellido_alum` varchar(45) NOT NULL,
+  `numerodocumento_alum` varchar(15) NOT NULL,
+  `nacionalidad_alum` varchar(45) NOT NULL,
+  `correo_alum` varchar(45) NOT NULL,
+  `celular_alum` varchar(15) NOT NULL,
   `fechanacimiento_alum` date NOT NULL,
   PRIMARY KEY (`codigo_alum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -55,11 +55,11 @@ DROP TABLE IF EXISTS `tb_curso`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_curso` (
-  `codigo_curso` int NOT NULL AUTO_INCREMENT,
+  `codigo_curso` varchar(5) NOT NULL,
   `nombre_curso` varchar(45) NOT NULL,
   `fechinicio_curso` date NOT NULL,
   `fechfin_curso` date NOT NULL,
-  `codigo_prof` int DEFAULT NULL,
+  `codigo_prof` varchar(5) NOT NULL,
   PRIMARY KEY (`codigo_curso`),
   KEY `fk_tb_curso_tb_profesor1_idx` (`codigo_prof`),
   CONSTRAINT `fk_tb_curso_tb_profesor1` FOREIGN KEY (`codigo_prof`) REFERENCES `tb_profesor` (`codigo_prof`)
@@ -72,7 +72,38 @@ CREATE TABLE `tb_curso` (
 
 LOCK TABLES `tb_curso` WRITE;
 /*!40000 ALTER TABLE `tb_curso` DISABLE KEYS */;
+INSERT INTO `tb_curso` VALUES ('C0001','Diseño web','2023-06-21','2023-08-21','P0002'),('C0002','Arquitectura de redes','2023-06-21','2023-08-21','P0001');
 /*!40000 ALTER TABLE `tb_curso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_matricula`
+--
+
+DROP TABLE IF EXISTS `tb_matricula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_matricula` (
+  `codigo_matric` varchar(5) NOT NULL,
+  `fecha_matric` date NOT NULL,
+  `codigo_alum` varchar(5) NOT NULL,
+  `codigo_curso` varchar(5) NOT NULL,
+  PRIMARY KEY (`codigo_matric`),
+  KEY `fk_tb_matricula_tb_alumno1_idx` (`codigo_alum`),
+  KEY `fk_tb_matricula_tb_curso1_idx` (`codigo_curso`),
+  CONSTRAINT `fk_tb_matricula_tb_alumno1` FOREIGN KEY (`codigo_alum`) REFERENCES `tb_alumno` (`codigo_alum`),
+  CONSTRAINT `fk_tb_matricula_tb_curso1` FOREIGN KEY (`codigo_curso`) REFERENCES `tb_curso` (`codigo_curso`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_matricula`
+--
+
+LOCK TABLES `tb_matricula` WRITE;
+/*!40000 ALTER TABLE `tb_matricula` DISABLE KEYS */;
+INSERT INTO `tb_matricula` VALUES ('M0001','2023-06-21','A0001','C0001'),('M0002','2023-06-21','A0001','C0002');
+/*!40000 ALTER TABLE `tb_matricula` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -83,7 +114,7 @@ DROP TABLE IF EXISTS `tb_profesor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_profesor` (
-  `codigo_prof` int NOT NULL AUTO_INCREMENT,
+  `codigo_prof` varchar(5) NOT NULL,
   `nombre_prof` varchar(45) NOT NULL,
   `apellido_prof` varchar(45) NOT NULL,
   `numerodocumento_prof` varchar(15) NOT NULL,
@@ -92,7 +123,7 @@ CREATE TABLE `tb_profesor` (
   `correo_prof` varchar(45) NOT NULL,
   `espe_prof` varchar(45) NOT NULL,
   PRIMARY KEY (`codigo_prof`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +132,7 @@ CREATE TABLE `tb_profesor` (
 
 LOCK TABLES `tb_profesor` WRITE;
 /*!40000 ALTER TABLE `tb_profesor` DISABLE KEYS */;
-INSERT INTO `tb_profesor` VALUES (1,'Julio','aaaaaaa','aaaaaaaa','aaaaaaaa','aaaaaaa','aaaa','aaaaaaaa'),(2,'222','222','222','22','22','2','2'),(3,'333','333','33','3','3','3','3'),(4,'444','44','4','44','4','4','4'),(6,'666','66','66','66','66','66','66'),(8,'77','77','77','77','77','77','77');
+INSERT INTO `tb_profesor` VALUES ('P0001','Robert','Chavez','12345678','Peruana','987654321','rchavez@gmail.com','Ciberseguridad'),('P0002','Maria','Fuertes','12345678','Extranjero','987654321','mfuertes@gmail.com','Diseño grafico');
 /*!40000 ALTER TABLE `tb_profesor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,4 +153,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-27 11:13:55
+-- Dump completed on 2023-06-21 12:10:26
